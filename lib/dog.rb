86691d61d44f8a6d1aa10bf0e7e299962b2c9b73
Caroline Berg <carolineeberg@gmail.com>
2018-07-25 14:47:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,24 @@
 class Dog
+  
   def initialize(name, breed)
     @name = name
     @breed = "Mutt"
   end
+  
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = "Mutt"
+  end
+
+  def breed
+    @breed
+  end
+
 end
